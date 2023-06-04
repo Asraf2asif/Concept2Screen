8d9bfff37bba80d3b46f11a5ee05fd97a045fa66
Asraf2asif <asraf2asif@gmail.com>
2023-06-04 19:11:26 +0000

    simple edit

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-import { OPENAI_API_KEY } from './env.js';
+import { OPENAI_API_KEY } from "./env.js";
+import openai from "https://cdn.jsdelivr.net/npm/openai@3.2.1/+esm";
 
-const setupTextarea = document.getElementById('setup-textarea')
-const setupInputContainer = document.getElementById('setup-input-container')
-const movieBossText = document.getElementById('movie-boss-text')
+const { Configuration, OpenAIApi } = openai;
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+const openaiApi = new OpenAIApi(configuration);
+
+const setupTextarea = document.getElementById("setup-textarea");
+const setupInputContainer = document.getElementById("setup-input-container");
+const movieBossText = document.getElementById("movie-boss-text");
 
 document.getElementById("send-btn").addEventListener("click", () => {
   if (setupTextarea.value) {
-    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
-    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
+    fetchBotReply();
   }
-})
+});
 
-console.log('OPENAI_API_KEY:', OPENAI_API_KEY);
+async function fetchBotReply() {
+  const response = await openaiApi
+    .createCompletion({
+      model: "text-davinci-003",
+      prompt: "Say this is a test",
+      temperature: 0,
+      max_tokens: 7,
+    })
+  movieBossText.innerText = response.choices[0].text.trim();
+}
