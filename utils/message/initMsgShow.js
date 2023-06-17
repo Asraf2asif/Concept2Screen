93404029247f4afc151e34c21e1a0996b5479878
Asif <asraf2asif@gmail.com>
2023-06-17 23:23:55 +0600

    Update initMsgShow.js

--- utils/message/initMsgShow.js
+++ utils/message/initMsgShow.js
@@ -5,11 +5,13 @@ import { typeTextByChar } from "../ui/typeTextByChar";
 // Get the necessary DOM elements
 const movieBossText = document.getElementById("movie-boss-text"); // Output text container
 
+const defaultMsg = "Give me a one-sentence concept and I'll give you an eye-catching title, a synopsis the studios will love, a movie poster... AND choose the cast!";
+
 export function initMsgShow() {
   loadingState();
   // Initial message
   typeTextByChar(
-    "Give me a one-sentence concept and I'll give you an eye-catching title, a synopsis the studios will love, a movie poster... AND choose the cast!",
+    defaultMsg,
     movieBossText,
     0,
     resetLoading
