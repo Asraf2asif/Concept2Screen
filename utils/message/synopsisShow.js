87a3c052c0a76fcbf31dff641d8a09805c9a246d
Asraf2asif <asraf2asif@gmail.com>
2023-06-21 23:11:14 +0600

    refactor, commenting and organization

--- utils/message/synopsisShow.js
+++ utils/message/synopsisShow.js
@@ -13,32 +13,50 @@ const inputContainer = document.getElementById("input-container"); // Input cont
 const movieBossText = document.getElementById("movie-boss-text"); // Movie boss text element
 const inputText = document.getElementById("input-text"); // Input Text
 
-export async function synopsisShow() {
-  loadingState(); // Set loading state
+// Function to show a message in the output container
+const showMessage = (text) => {
+  const messageElement = document.createElement("p"); // Create a new paragraph element
+  outputContainer.appendChild(messageElement); // Append the message element to the output container
+  typeTextByChar(text, messageElement); // Type the text character by character in the message element
+  return messageElement; // Return the created message element
+};
 
-  inputContainer.style.display = "block"; // Show input container
-  inputText.textContent = setupTextarea.value; // Set input text
+// Function to remove a message from the output container
+const removeMessage = (messageElement) => {
+  if (messageElement && messageElement.parentNode) {
+    outputContainer.removeChild(messageElement); // Remove the message element from the output container
+  }
+};
 
-  outputContainer.style.display = "block"; // Show output container
+// Function to show a message for synopsis generation
+const showSynopsisGenerationMessage = () => {
+  const messageElement = showMessage(
+    "Generating movie synopsis based on your concept..."
+  );
+  return messageElement;
+};
 
-  const showMessage = (text) => {
-    const messageElement = document.createElement("p"); // Create a new paragraph element
-    outputContainer.appendChild(messageElement); // Append the message element to the output container
-    typeTextByChar(text, messageElement); // Type the text character by character in the message element
-    return messageElement; // Return the created message element
-  };
+// Function to show a message for synopsis generated
+const showSynopsisGeneratedMessage = () => {
+  const messageElement = showMessage(
+    "✔️ Movie synopsis generated based on your concept..."
+  );
+  return messageElement;
+};
 
-  const removeMessage = (messageElement) => {
-    if (messageElement && messageElement.parentNode) {
-      outputContainer.removeChild(messageElement); // Remove the message element from the output container
-    }
-  };
+// Function to show a message for title generation
+const showTitleGenerationMessage = () => {
+  const messageElement = showMessage(
+    "✔️ Generating movie synopsis based on your concept..."
+  );
+  return messageElement;
+};
 
+// Function to generate the synopsis
+const generateSynopsis = async () => {
   const { synopsisData } = data; // Get the synopsis data from the imported data
 
-  const messageElement1 = showMessage(
-    "Generating movie synopsis based on your concept..."
-  ); // Show the first message
+  const messageElement1 = showSynopsisGenerationMessage(); // Show the message indicating synopsis generation
 
   const synopsis = await fetchBotReply({
     dataArray: synopsisData,
@@ -49,30 +67,52 @@ export async function synopsisShow() {
     outputContainerElement: outputContainer,
   }); // Fetch the synopsis
 
-  removeMessage(messageElement1); // Remove the first message
+  removeMessage(messageElement1); // Remove the message indicating synopsis generation
+
+  return synopsis;
+};
+
+// Function to generate the movie title
+const generateMovieTitle = async (synopsis) => {
+  const messageElement2 = showSynopsisGeneratedMessage(); // Show the message indicating synopsis generation is complete
+
+  const movieTitle = await fetchBotReply({
+    prompt: `Generate a catchy movie title for this synopsis\n###${synopsis}`,
+    max_tokens: 700,
+    outputTextElement: outputTitle,
+    outputContainerElement: outputContainer,
+  }); // Fetch the movie title based on the synopsis
+
+  removeMessage(messageElement2); // Remove the message indicating synopsis generation is complete
+
+  return movieTitle;
+};
+
+// Function to show the final title generation message
+const showFinalTitleGenerationMessage = () => {
+  const messageElement3 = showTitleGenerationMessage(); // Show the final message indicating title generation
+
+  setTimeout(() => {
+    removeMessage(messageElement3); // Remove the final message after a short delay
+  }, 10);
+};
+
+// Main function to display the synopsis and movie title
+export async function synopsisShow() {
+  loadingState(); // Set loading state
+
+  inputContainer.style.display = "block"; // Show input container
+  inputText.textContent = setupTextarea.value; // Set input text
+
+  outputContainer.style.display = "block"; // Show output container
+
+  const synopsis = await generateSynopsis(); // Generate the synopsis
 
   if (synopsis) {
-    const messageElement2 = showMessage(
-      "✔️ Movie synopsis generated based on your concept..."
-    ); // Show the second message
-
-    const movieTitle = await fetchBotReply({
-      prompt: `Generate a catchy movie title for this synopsis\n###${synopsis}`,
-      max_tokens: 700,
-      outputTextElement: outputTitle,
-      outputContainerElement: outputContainer,
-    }); // Fetch the movie title based on the synopsis
-
-    removeMessage(messageElement2); // Remove the second message
+    const movieTitle = await generateMovieTitle(synopsis); // Generate the movie title based on the synopsis
 
     if (movieTitle) {
-      const messageElement3 = showMessage(
-        "✔️ Generating movie synopsis based on your concept..."
-      ); // Show the third message
-
-      setTimeout(() => {
-        removeMessage(messageElement3); // Remove the third message after a short delay
-      }, 10);
+      showFinalTitleGenerationMessage(); // Show the final message indicating title generation is complete
 
       outputTitle.style.display = "block"; // Show the output title
       typeTextByChar(movieTitle, outputTitle); // Type the movie title character by character in the output title
